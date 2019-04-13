714ded0456dad8e74d46bbfbf2825cab511edebc
Emil <langgezockt@gmail.com>
2019-04-13 07:37:06 +0200

    added reload and remove argument

--- src/main/java/de/langgezockt/Prefix/commands/PrefixCommand.java
+++ src/main/java/de/langgezockt/Prefix/commands/PrefixCommand.java
@@ -26,26 +26,65 @@ public class PrefixCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        String USAGE = CC.createUsage(label, "[add | remove] <name> <prefix>");
+        String USAGE = CC.createUsage(label, "[reload | add | remove] <name> <prefix>");
 
-        if(!(sender instanceof Player)) {
+        if (!(sender instanceof Player)) {
             sender.sendMessage(instance.getMessages().get("Player-Only"));
             return true;
         }
 
         Player player = (Player) sender;
 
-        if(args.length == 0) {
+        if (args.length == 0) {
             instance.getPrefixManager().getPrefixInventory().createInventories(player);
             instance.getPrefixManager().getPrefixInventory().openFirstPage(player);
             return true;
         }
-        if(args.length <= 2) {
+
+        if (args.length == 1) {
+            if (!(args[0].equalsIgnoreCase("reload"))) {
+                player.sendMessage(USAGE);
+                return true;
+            }
+
+            if (!(player.hasPermission("prefix.reload"))) {
+                player.sendMessage(instance.getMessages().get("No-Permissions"));
+                return true;
+            }
+
+            instance.getPrefixManager().loadPrefixes();
+            player.sendMessage(instance.getMessages().get("Prefix.Reload"));
+        }
+
+        if (args.length == 2) {
+            if (!(args[0].equalsIgnoreCase("remove"))) {
+                player.sendMessage(USAGE);
+                return true;
+            }
+
+            if (!(player.hasPermission("prefix.remove"))) {
+                player.sendMessage(instance.getMessages().get("No-Permissions"));
+                return true;
+            }
+
+            String name = args[1];
+
+            instance.getPrefixManager().removePrefix(name);
+            player.sendMessage(instance.getMessages().get("Prefix.Remove").replaceAll("%name%", name));
+            return true;
+        }
+
+        if (args.length < 3) {
             player.sendMessage(USAGE);
             return true;
         }
 
-        if(!(player.hasPermission("prefix.add"))) {
+        if (!(args[0].equalsIgnoreCase("add"))) {
+            player.sendMessage(USAGE);
+            return true;
+        }
+
+        if (!(player.hasPermission("prefix.add"))) {
             player.sendMessage(instance.getMessages().get("No-Permissions"));
             return true;
         }
