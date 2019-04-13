86a12363dc97b517a4a11f1d3f2d61f774d25552
Emil <langgezockt@gmail.com>
2019-04-13 06:42:53 +0200

    added methods

--- src/main/java/de/langgezockt/Prefix/utils/ConfigurationService.java
+++ src/main/java/de/langgezockt/Prefix/utils/ConfigurationService.java
@@ -1,5 +1,12 @@
 package de.langgezockt.Prefix.utils;
 
+import de.langgezockt.Prefix.Prefix;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author langgezockt (langgezockt@gmail.com)
  * 13.04.2019 / 05:38
@@ -8,6 +15,36 @@ package de.langgezockt.Prefix.utils;
 
 public class ConfigurationService {
 
-    public static boolean MYSQL_ENABLED;
+
+    private Prefix instance;
+    private File configFile;
+    private FileConfiguration config;
+
+    public ConfigurationService(Prefix instance) {
+        this.instance = instance;
+        configFile = new File(instance.getDataFolder(), "config.yml");
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public static String STORE_URL;
+
+    public void setDefaults() {
+        this.config.options().copyDefaults(true);
+        this.config.addDefault("Store-URL", "store.yourserver.com");
+
+    }
+
+    public void load() {
+        STORE_URL = this.config.getString("Store-URL");
+    }
+
+    public void save() {
+        try {
+            this.config.save(this.configFile);
+        } catch (IOException e) {
+            instance.getLogger().warning("Failed to save config.yml stacktrace below: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
 }
