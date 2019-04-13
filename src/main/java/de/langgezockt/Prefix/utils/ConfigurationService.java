c49cdcb0e8831bf8c7d113985b4e5f452c7495fc
Emil <langgezockt@gmail.com>
2019-04-13 05:40:10 +0200

    added ConfigurationService

---
+++ src/main/java/de/langgezockt/Prefix/utils/ConfigurationService.java
@@ -0,0 +1,13 @@
+package de.langgezockt.Prefix.utils;
+
+/**
+ * @author langgezockt (langgezockt@gmail.com)
+ * 13.04.2019 / 05:38
+ * Prefix / de.langgezockt.Prefix.utils
+ */
+
+public class ConfigurationService {
+
+    public static boolean MYSQL_ENABLED;
+
+}
